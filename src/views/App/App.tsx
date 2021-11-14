f529d2c0091be7010defe3d2d2344ee6c1dd4d30
Luis Isasi <luis16isasi@gmail.com>
2021-11-14 15:04:55 -0500

    style: add style for show user's data

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -14,14 +14,20 @@ const App = () => {
   if (error) return <div>Error</div>
 
   const { results } = data
+
   return (
-    <div className="">
-      <p>
-        Name: {results[0].name.first} <br />
-        Last Name: {results[0].name.last} <br />
-        Email: {results[0].email} <br />
-        Number: {results[0].phone} <br />
-      </p>
+    <div className="h-screen bg-gray-800 flex justify-center items-center">
+      <div className="px-4 md:px-0 flex flex-col items-center text-white">
+        <h1 className="text-4xl lg:text-5xl font-bold mb-12 text-center">
+          Ramdom User 😀
+        </h1>
+        <p className="text-base  lg:text-xl">
+          <b>Name:</b> {results[0].name.first} <br />
+          <b>Last Name:</b> {results[0].name.last} <br />
+          <b>Email:</b> {results[0].email} <br />
+          <b>Number:</b> {results[0].phone} <br />
+        </p>
+      </div>
     </div>
   )
 }
